7816856bb3c1da29ea587348b582885dc858fe29
amnugian <flimcream2411@gmail.com>
2025-11-01 13:54:34 +0700

    addedRandomization-v1.03

--- code.py
+++ code.py
@@ -1,4 +1,9 @@
+import random
 listofNumbers = [3, 4, 5]
+
+for i in range(1, random.randint(1, 100+1)):
+    newRandomInt = random.randint(1, 1000)
+    listofNumbers.append(newRandomInt)
 
 newNumber = 0
 for i in listofNumbers:
