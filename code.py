92164a525541bac226e3881d4aed069bcec4fe23
amnugian <flimcream2411@gmail.com>
2025-11-01 13:59:28 +0700

    addedComments-v1.04

--- code.py
+++ code.py
@@ -1,12 +1,25 @@
 import random
-listofNumbers = [3, 4, 5]
 
-for i in range(1, random.randint(1, 100+1)):
-    newRandomInt = random.randint(1, 1000)
-    listofNumbers.append(newRandomInt)
+# -- List of numbers that will be processed 
+listofNumbers = [3, 4, 5] 
 
-newNumber = 0
-for i in listofNumbers:
-    newNumber += i
+# -- Runs a random amount of times, from 1 to 1000
+for i in range(1, random.randint(1, 100+1)): 
     
-print("Here is the new number: "+str(newNumber))
+     # -- Picks a number from 1 to 1000 and stores it
+    newRandomInt = random.randint(1, 1000)
+    
+    # -- Appends the new number to the list of to-be processed numbers
+    listofNumbers.append(newRandomInt) 
+
+# -- Initializes the final unprocessed product as 1
+newNumber = 0 
+
+# -- Runs through each number in the to-be processed numbers list
+for i in listofNumbers: 
+    
+    # -- Adds the to-be processed number to the final product
+    newNumber += i 
+    
+# -- Prints the final product
+print("Here is the new number: "+str(newNumber)) 
